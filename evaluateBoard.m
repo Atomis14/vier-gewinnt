6733c3a11a1ce75d2b18c98e4cfc22107f362fca
Simona <simona@Simona-Borghi-MacBook-Air.local>
2019-12-20 11:09:54 +0100

    x

--- evaluateBoard.m
+++ evaluateBoard.m
@@ -1,4 +1,16 @@
 function evaluateBoard()
+%Hat jemand gewonnen? Geraden und Diagonalen überprüfen und ob Spielfeld
+%voll
+% Spielfeld-Matrize als Übergabewert
+%TODO Reihen überprüfen
+
+
+%TODO Kolonnen überprüfen
+
+
+%TODO Diagonalen überprüfen
+
+
 matri = [1 2 3 4];
 matri
 return
